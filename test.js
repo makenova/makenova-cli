8e2864cb015efa64c248c51b44f28ee93a79a679
makenova <themarchmyth@gmail.com>
2016-12-08 00:20:49 -0600

    Add tests

--- test.js
+++ test.js
@@ -12,8 +12,23 @@ function readfile(path) {
   })
 }
 
+function parse(string) {
+  return new Promise((resolve, reject) => {
+    try {
+      let json = JSON.parse(string)
+      return resolve(json)
+    } catch (e) {
+      return reject(e)
+    }
+  })
+}
+
 const name = `Mayiawo Aken'Ova`
 const title = `Code pusher`
+const alias = `makenova`
+const twitter = `@make_nova`
+const github = `https://github.com/makenova`
+const website = `http://makenova.com`
 
 test('test name', t => {
   const makenova = new Makenova()
@@ -23,6 +38,26 @@ test('test name', t => {
 test('test title', t => {
   const makenova = new Makenova()
   t.is(title, makenova.title)
+})
+
+test('test alias', t => {
+  const makenova = new Makenova()
+  t.is(alias, makenova.alias)
+})
+
+test('test twitter', t => {
+  const makenova = new Makenova()
+  t.is(twitter, makenova.twitter)
+})
+
+test('test github', t => {
+  const makenova = new Makenova()
+  t.is(github, makenova.github)
+})
+
+test('test website', t => {
+  const makenova = new Makenova()
+  t.is(website, makenova.website)
 })
 
 test('test cli name', t => {
@@ -37,6 +72,12 @@ test('test cli title', t => {
 
 test('test cli default no args', t => t.throws(execa('.cli.js')))
 
-test.todo('test version', t => {
-
+test('test cli version', t => {
+  return Promise.all([
+    readfile('./package.json').then(parse).then(config => config.version),
+    execa.stdout('./cli.js',  ['--version'])
+  ])
+    .then(results => {
+      t.is(results[0], results[1])
+    })
 })
